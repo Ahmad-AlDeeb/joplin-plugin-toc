0b7d974788d076e8d0ab87d2cd1538c09e6bb53a
Ahmad Al-Deeb <ahmeddeeb22222@gmail.com>
2024-02-29 09:15:16 +0200

    Implement noteheaders & headerSlug

--- src/index.ts
+++ src/index.ts
@@ -15,6 +15,7 @@ joplin.plugins.register({
 
 			// Keep in mind that it can be `null` if nothing is currently selected!
 			if (note) {
+					const headers = noteHeaders(note.body);
 					console.info('Note content has changed! New note is:', note);
 			} else {
 					console.info('No note is selected');
@@ -37,3 +38,33 @@ joplin.plugins.register({
 	},
 });
 
+// Extract the headers from that note in order to build the Table Of Content from it
+// Quick and dirty solution is to get all the lines that start with any number of # followed by a space
+// Return an array of headers, with the text and level (H1, H2, etc.) of header:
+function noteHeaders(noteBody:string) {
+	const headers = [];
+	const lines = noteBody.split('\n');
+	for (const line of lines) {
+			const match = line.match(/^(#+)\s(.*)*/);
+			if (!match) continue;
+			headers.push({
+					level: match[1].length,
+					text: match[2],
+			});
+	}
+	return headers;
+}
+// To generate the slug for each header
+// A slug is an identifier which is used to link to a particular header.
+// Essentially a header text like "My Header" is converted to "my-header"
+// If there's already a slug with that name, a number is appended to it.
+const uslug = require('@joplin/fork-uslug');
+let slugs = {};
+function headerSlug(headerText) {
+    const s = uslug(headerText);
+    let num = slugs[s] ? slugs[s] : 1;
+    const output = [s];
+    if (num > 1) output.push(num);
+    slugs[s] = num + 1;
+    return output.join('-');
+}
